056614d18fb74f77e4fedca267456fbfb930617b
Aritra Banik <ar1trabanik@Aritras-MacBook-Air.local>
2024-02-17 18:36:58 +0530

    004

--- core/database.ts
+++ core/database.ts
@@ -2,11 +2,20 @@ const sqlite3 = require('sqlite3').verbose();
 
 // let db = new sqlite3.Database(':memory:');
 
-let db = new sqlite3.Database(':memory:', (err: { message: any; }) => {
+let db = new sqlite3.Database('../db/normal.db');
+
+let sql = `CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+  )`;
+
+db.all(sql, [], (err: any, rows: any[]) => {
   if (err) {
-    return console.error(err.message);
+    throw err;
   }
-  console.log('Connected to the in-memory SQlite database.');
+  rows.forEach((row: { name: any; }) => {
+    console.log(row.name);
+  });
 });
 
 db.close((err: { message: any; }) => {
