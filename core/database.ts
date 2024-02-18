1af854229b8e8ba87bd788238ada1ced93124c0c
Aritra Banik <ar1trabanik@Aritras-MacBook-Air.local>
2024-02-18 15:28:35 +0530

    004

--- core/database.ts
+++ core/database.ts
@@ -4,6 +4,11 @@ const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('../db/normal.db');
 
+export function dbConn(): any {
+  let db = new sqlite3.Database('../db/normal.db');
+  return db
+}
+
 let sql = `CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT
