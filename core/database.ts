6a958a433eb427ffb3b799360303642c75a09944
Aritra Banik <ar1trabanik@Aritras-MacBook-Air.local>
2024-02-18 15:49:24 +0530

    006

--- core/database.ts
+++ core/database.ts
@@ -1,7 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// let db = new sqlite3.Database(':memory:');
-
 let db = new sqlite3.Database('../db/normal.db');
 
 let sql = `CREATE TABLE IF NOT EXISTS users (
@@ -9,23 +7,47 @@ let sql = `CREATE TABLE IF NOT EXISTS users (
   name TEXT
   )`;
   
-  db.all(sql, [], (err: any, rows: any[]) => {
+db.all(sql, [], (err: any, rows: any[]) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row: { name: any; }) => {
+    console.log(row.name);
+  });
+});
+
+db.close((err: { message: any; }) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+export function dbConn(): any {
+  let db = new sqlite3.Database('../db/normal.db');
+  return db
+}
+
+// function createTable creates a table in the database as named in the argument and the provided map of columns
+export function createTable(db: any, tableName: string, columns: { [x: string]: string; }): void {
+  let sql = `CREATE TABLE IF NOT EXISTS ${tableName} (`;
+  for (const [key, value] of Object.entries(columns)) {
+    sql += `${key} ${value}, `;
+  }
+  sql = sql.slice(0, -2);
+  sql += ')';
+  db.run(sql, (err: any) => {
     if (err) {
       throw err;
     }
-    rows.forEach((row: { name: any; }) => {
-      console.log(row.name);
-    });
   });
-  
+}
+
+export function dbClose(db: any): void {
   db.close((err: { message: any; }) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Close the database connection.');
   });
-
-  export function dbConn(): any {
-    let db = new sqlite3.Database('../db/normal.db');
-    return db
-  }
+}
