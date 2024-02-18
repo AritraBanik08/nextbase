4c1ab4c2a90d15b1b4415d907dae89434983a50c
Aritra Banik <ar1trabanik@Aritras-MacBook-Air.local>
2024-02-18 15:34:51 +0530

    005

--- core/database.ts
+++ core/database.ts
@@ -4,28 +4,28 @@ const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('../db/normal.db');
 
-export function dbConn(): any {
-  let db = new sqlite3.Database('../db/normal.db');
-  return db
-}
-
 let sql = `CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT
   )`;
-
-db.all(sql, [], (err: any, rows: any[]) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach((row: { name: any; }) => {
-    console.log(row.name);
+  
+  db.all(sql, [], (err: any, rows: any[]) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row: { name: any; }) => {
+      console.log(row.name);
+    });
   });
-});
+  
+  db.close((err: { message: any; }) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 
-db.close((err: { message: any; }) => {
-  if (err) {
-    return console.error(err.message);
+  export function dbConn(): any {
+    let db = new sqlite3.Database('../db/normal.db');
+    return db
   }
-  console.log('Close the database connection.');
-});
